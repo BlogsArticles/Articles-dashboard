99d35aa3c4914cd65399172d26330c5456bd8296
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-14 22:45:29 +0200

    add delete route and cotroller

---
+++ Http/controllers/groups/destroy.php
@@ -0,0 +1,21 @@
+<?php
+use Core\App;
+use Core\Database;
+use Core\Validator;
+$db = App::resolve(Database::class);
+
+
+if(!isset($_POST["id"])){
+    abort(404);
+}
+
+$group = $db->query('select * from articles_blog.groups where id= :id',[
+    "id"=>$_POST["id"]
+])->findOrFail();
+
+
+$db->query('delete from articles_blog.groups where id= :id',[
+    "id"=>$_POST["id"]
+]);
+
+header('location: /groups');
