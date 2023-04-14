56f61cf66e73ef12d63d0164bc3f444d327c6081
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-14 15:09:17 +0200

    create login routers

--- routes.php
+++ routes.php
@@ -17,6 +17,6 @@ $router->post('/notes', 'notes/store.php');
 $router->get('/register', 'registration/create.php')->only('guest');
 $router->post('/register', 'registration/store.php')->only('guest');
 
-$router->get('/login', 'session/create.php')->only('guest');
-$router->post('/session', 'session/store.php')->only('guest');
-$router->delete('/session', 'session/destroy.php')->only('auth');
+$router->get('/login', 'login/index.php');
+$router->post('/login', 'login/store.php');
+
