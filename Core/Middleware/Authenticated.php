b32428893e612edfb26d549124e83e37d725aac3
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-14 23:37:12 +0200

    create function remember values

--- Core/Middleware/Authenticated.php
+++ Core/Middleware/Authenticated.php
@@ -6,7 +6,7 @@ class Authenticated
 {
     public function handle()
     {
-        if (! $_SESSION['user'] ?? false) {
+        if (! $_SESSION['user_id'] ?? false) {
             header('location: /');
             exit();
         }
