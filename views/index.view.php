761db36649401c67f2c3239aeed2f06e94966133
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-04-14 18:49:32 +0200

    some refactor

--- views/index.view.php
+++ views/index.view.php
@@ -5,30 +5,27 @@
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
                         <h1 class="m-0">Dashboard</h1>
-                    </div><!-- /.col -->
+                    </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
                         </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
+                    </div>
+                </div>
+            </div>
         </div>
-        <!-- /.content-header -->
 
-    <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
             <h1>hello</h1>
-        </div><!-- /.container-fluid -->
+        </div>
     </section>
-    <!-- /.content -->
+
 </div>
 
 
