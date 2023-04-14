4a96dc51ac60cd005ffeef75f5350207d06e4a1f
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-15 00:17:25 +0200

    add search to groups

--- Http/controllers/groups/index.php
+++ Http/controllers/groups/index.php
@@ -2,8 +2,14 @@
 use Core\App;
 use Core\Database;
 $db = App::resolve(Database::class);
+// dd($_GET);
+if(isset($_GET["search"])){
+    $groups = $db->query('select * from articles_blog.groups where name like :search or description like :search',["search"=>'%'.$_GET["search"].'%'])->get();
+    
+}else{
+    $groups = $db->query('select * from articles_blog.groups')->get();
 
-$groups = $db->query('select * from articles_blog.groups')->get();
+}
 // dd($groups);
 view("groups/index.view.php",[
     "groups"=>$groups
