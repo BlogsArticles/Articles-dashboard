ae38c99f42324ae4b2adb2c0f3d3093934a0e681
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-15 17:37:32 +0200

    init create.articles view

---
+++ views/articles/create.view.php
@@ -0,0 +1,37 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/sidebar.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Create articles</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/">home</a></li>
+                        <li class="breadcrumb-item"><a href="/articles">articles</a></li>
+                        <li class="breadcrumb-item">create article</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+
+
+<?php require base_path('views/partials/footer.php') ?>
