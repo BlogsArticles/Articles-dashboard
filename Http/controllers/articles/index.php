3de7221692df06fa4c632318b5e0288bc5f7d4f3
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-15 15:56:48 +0200

    Editing index view-articles

--- Http/controllers/articles/index.php
+++ Http/controllers/articles/index.php
@@ -1,5 +1,27 @@
 <?php
 
+use Core\App;
+use Core\Database;
+
+
+$db = App::resolve(Database::class);
+$recordsPerPage = 1;
+$page = $_GET['page'] ?? 1;
+$offset = ($page - 1) * $recordsPerPage;
+
+$articles = $db
+    ->query("select * from articles where is_deleted IS NULL LIMIT {$offset} , {$recordsPerPage}")
+    ->get();
+
+foreach ($articles as &$article){
+    $article['summary'] = resizeStingAndAppend( $article['summary'] );
+}
+
+
+
 view("articles/index.view.php", [
-    'errors' => []
+    'errors' => [],
+    'articles' => $articles,
+    'recordsPerPage' => $recordsPerPage,
+    'page' => $page
 ]);
