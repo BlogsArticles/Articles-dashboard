72e9ac31e05d9476a9589430ee3ceb1867df8fb0
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-15 20:58:48 +0200

    transfer groups/update validation to Requests/UpdateGroupsRequests.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => 'blosgs',
+        'dbname' => 'blogs',
         'charset' => 'utf8mb4'
     ],
     'username' => 'root',
