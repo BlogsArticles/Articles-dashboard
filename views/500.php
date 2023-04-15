198c617e58994cb0ce37d2603c47c7fb01d5e2e9
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-16 00:28:08 +0200

    create 500 error page

---
+++ views/500.php
@@ -0,0 +1,40 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/sidebar.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
+
+<div class="content-wrapper" style="min-height: 2838.44px;">
+
+<section class="content-header">
+<div class="container-fluid">
+<div class="row mb-2">
+<div class="col-sm-6">
+<h1>500 Error Page</h1>
+</div>
+<div class="col-sm-6">
+<ol class="breadcrumb float-sm-right">
+<li class="breadcrumb-item"><a href="/">Home</a></li>
+<li class="breadcrumb-item active">500 Error Page</li>
+</ol>
+</div>
+</div>
+</div>
+</section>
+
+<section class="content">
+<div class="error-page">
+<h2 class="headline text-danger"> 500</h2>
+<div class="error-content">
+<h3><i class="fas fa-exclamation-triangle text-danger"></i> 500 Internal Server Error.</h3>
+<p>
+Sorry, something went wrong on the server. Our team has been notified and we are working to fix the problem. Please try again later.
+</p>
+</div>
+
+</div>
+
+</section>
+
+</div>
+
+<?php require base_path('views/partials/footer.php') ?>
