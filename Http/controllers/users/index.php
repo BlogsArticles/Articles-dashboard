748d5c86d832bbc5c3e43202412afb5c14fb2606
omaralaa <omaralaa0989@gmail.com>
2023-04-15 16:53:41 +0200

    modify sql query to get group name

--- Http/controllers/users/index.php
+++ Http/controllers/users/index.php
@@ -4,12 +4,14 @@ use Core\App;
 use Core\Database;
 
 $db = App::resolve(Database::class);
-$users = $db->query('select id,name,email,phone from users')->get();
+$users = $db->query('select u.* ,g.name as group_name  from users u ,blog.groups g where u.group_id= g.id;')->get();
+
+
 
 var_dump($users);
 
 view("users/index.view.php", [
-    'heading' => 'My Notes',
+   
     'users' => $users
 ]);
 
--- views/users/index.view.php
+++ views/users/index.view.php
@@ -3,61 +3,59 @@
 <?php require base_path('views/partials/sidebar.php') ?>
 <?php require base_path('views/partials/banner.php') ?>
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Dashboard</h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Dashboard</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
-        <table class="table table-hover text-nowrap">
-<thead>
-<tr>
-<th>ID</th>
-<th>name</th>
-<th>email</th>
-<th>Contacts</th>
-<th>Edit</th>
-<th>Delete</th>
-</tr>
-</thead>
-<tbody>
+            <table class="table table-hover text-nowrap">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>name</th>
+                        <th>email</th>
+                        <th>Contacts</th>
+                        <th>Group Name</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-<?php 
-foreach ($users as $user) {
-   echo "<tr>";
-   echo " <td>$user[id]</td>";
-   echo  "<td>$user[name]</td>";
-   echo  "<td>$user[email]</td>";
-   echo "<td>$user[phone]</td>";
-   echo "<td> <a href='/edit' class='nav-link active'><i class='fas fa-edit'></i></a> </td>";
+                    <?php
+                    foreach ($users as $user) {
+                        echo "<tr>";
+                        echo " <td>$user[id]</td>";
+                        echo  "<td>$user[name]</td>";
+                        echo  "<td>$user[email]</td>";
+                        echo "<td>$user[phone]</td>";
+                        echo "<td>$user[group_name]</td>";
+                        echo "<td> <a href='/edit' class='nav-link active'><i class='fas fa-edit'></i></a> </td>";
+                        echo "<td> <a href='/edit' class='nav-link active'><i class='fas fa-trash-alt'></i></a></td>";
+                        echo " </tr>";
+                    }
 
-   echo "<td> <a href='/edit' class='nav-link active'>
-   <i class='fas fa-trash-alt'></i>
-</a></td>";
-   echo " </tr>" ;
-   
-}
+                    ?>
 
-?>
-
-</tbody>
-</table>
+                </tbody>
+            </table>
 
 
         </div><!-- /.container-fluid -->
