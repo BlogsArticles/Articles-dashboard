f5bf975bb05e56d88ba39cc1a5d05947e062e6bd
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-15 18:13:08 +0200

    Add a article

--- views/articles/create.view.php
+++ views/articles/create.view.php
@@ -27,7 +27,34 @@
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
+            <div class="card card-primary">
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form action="/article" method="POST">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" value="" class="form-control" name="title" id="title" placeholder="Enter article title here">
+                    </div>
+                    <div class="form-group">
+                        <label for="building-number">Building number</label>
+                        <input type="text" value="" class="form-control" name="building_number" id="building-number" placeholder="Enter Building number">
+                    </div>
+                    <div class="form-group">
+                        <label for="floor-number">Floor number</label>
+                        <input type="text" value="" class="form-control" name="floor_number" id="floor-number" placeholder="Enter floor number">
+                    </div>
+                    <div class="form-group">
+                        <label for="flat_number">Flat number</label>
+                        <input type="text" value="" class="form-control" name="flat_number" id="flat-number" placeholder="Enter Flat number">
+                    </div>
+                <!-- /.card-body -->
 
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-dark w-100">Submit</button>
+                </div>
+                </form>
+            </div>
         </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
