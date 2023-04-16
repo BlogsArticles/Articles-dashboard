13e95f7d9f16d83b6b4bf3a6165f566fc9138d4b
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-16 22:38:52 +0200

    init the show.view without the image

---
+++ views/articles/show.view.php
@@ -0,0 +1,48 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/sidebar.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+<!--                    <h1 class="m-0">Dashboard</h1>-->
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item"><a href="/articles">articles</a></li>
+                        <li class="breadcrumb-item"><a href="#"><?= $article['id'] ?></a></li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container d-flex flex-column">
+            <h2 class="col-12 text-center"><?= $article['title'] ?></h2>
+            <div>
+                <img src="<?= $article['image'] ?>" class="img-fluid my-3">
+            </div>
+            <div>
+                <h4>Content</h4>
+                <p><?= $article['content'] ?></p>
+            </div>
+            <div>
+                <h4>Summary</h4>
+                <p class="lead"><?= $article['summary'] ?></p>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+
+
+<?php require base_path('views/partials/footer.php') ?>
