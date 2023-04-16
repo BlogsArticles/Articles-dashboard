ae2dce36fdc8b332e42d7f05948f95f6fcbff807
Ahmed Hamada <122265923+AhmedHamada011@users.noreply.github.com>
2023-04-16 22:34:09 +0200

    Update 500.php

--- views/500.php
+++ views/500.php
@@ -23,7 +23,7 @@
 
 <section class="content">
 <div class="error-page">
-<h2 class="headline text-warning"> 403</h2>
+<h2 class="headline text-warning"> 500</h2>
 <div class="error-content">
 <h3><i class="fas fa-exclamation-triangle text-warning"></i>oops , something went wrong.</h3>
 <p>
