b172f4f3e064b5b95bfa01757e48c35616055a9c
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-17 02:43:33 +0200

    Adding side-bar Articles

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -64,6 +64,23 @@
                         </li>
                     </ul>
                 </li>
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Articles
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="/articles" class="nav-link active">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>all articles</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
