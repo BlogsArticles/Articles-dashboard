61d4f9f0a9a638ee1bc49fb931a96a53f825fb43
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-17 03:57:44 +0200

    Changing capital letters

--- views/articles/index.view.php
+++ views/articles/index.view.php
@@ -15,7 +15,7 @@
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item"><a href="/">home</a></li>
                         <li class="breadcrumb-item">articles</li>
                     </ol>
                 </div><!-- /.col -->
