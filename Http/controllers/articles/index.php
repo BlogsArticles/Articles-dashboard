c7f4ba6c286e0f07500d477e54b60fda5b5d46c4
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-17 23:21:43 +0200

    Removing the pagination

--- Http/controllers/articles/index.php
+++ Http/controllers/articles/index.php
@@ -5,17 +5,12 @@ use Core\Database;
 
 
 $db = App::resolve(Database::class);
-$recordsPerPage = 10;
-$page = $_GET['page'] ?? 1;
-$offset = ($page - 1) * $recordsPerPage;
 
 $articles = $db
-    ->query("select * from articles where is_deleted IS NULL LIMIT {$offset} , {$recordsPerPage}")
+    ->query("select * from articles where is_deleted IS NULL")
     ->get();
 
 view("articles/index.view.php", [
     'errors' => [],
-    'articles' => $articles,
-    'recordsPerPage' => $recordsPerPage,
-    'page' => $page
+    'articles' => $articles
 ]);
--- config.php
+++ config.php
@@ -4,11 +4,11 @@ return [
     'database' => [
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => 'blogs',
+        'dbname' => 'blog',
         'charset' => 'utf8mb4'
     ],
     'username' => 'root',
-    'password' => 'qw1234554321',
+    'password' => '059825',
     'logs' => [
         'file' => BASE_PATH . 'storage/logs/log.txt',
     'EXPIRATION_DATE'=> 24 * 60 * 60
