1e56bdbd482bdb675a425282a12f778022169e97
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-24 01:08:14 +0200

    add pagination in groups

--- Http/controllers/groups/index.php
+++ Http/controllers/groups/index.php
@@ -10,25 +10,25 @@ Auth::only_admin();
 
 $db = App::resolve(Database::class);
 $start=0;
-$number=10;
+$offset=10;
 $max=0;
 $page=1;
 
 if(isset($_GET["page"]) && is_numeric($_GET["page"])){
     $page=intval($_GET["page"]);
-    $start=($page-1)*$number;
+    $start=($page-1)*$offset;
     
 }
 if(isset($_GET["search"])){
-    $max=$db->query('select count(*) from `groups` where is_deleted is null and (name like :search or description like :search)',["search"=>'%'.$_GET["search"].'%'])->find();
-    $max=ceil($max["count(*)"]/$number);
-    $groups = $db->query("select * from `groups` where is_deleted is null and (name like :search or description like :search) LIMIT  $start,$number",["search"=>'%'.$_GET["search"].'%'])->get();
+    $count=$db->query('select count(*) from `groups` where is_deleted is null and (name like :search or description like :search)',["search"=>'%'.$_GET["search"].'%'])->find();
+    $max=ceil($count["count(*)"]/$offset);
+    $groups = $db->query("select * from `groups` where is_deleted is null and (name like :search or description like :search) LIMIT  $start,$offset",["search"=>'%'.$_GET["search"].'%'])->get();
     
 }else{
-    $max=$db->query('select count(*) from `groups` where is_deleted is null')->find();
-    $max=ceil($max["count(*)"]/$number);
+    $count=$db->query('select count(*) from `groups` where is_deleted is null')->find();
+    $max=ceil($count["count(*)"]/$offset);
 
-    $groups = $db->query("select * from `groups` where is_deleted is null LIMIT $start,$number",[
+    $groups = $db->query("select * from `groups` where is_deleted is null LIMIT $start,$offset",[
         
     ])->get();
 }
