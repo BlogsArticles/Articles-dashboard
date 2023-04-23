82013bdf90feaccd1208ece39d7ef5dfa7e2df85
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-23 22:47:05 +0200

    remove dbname from select statment

--- Http/controllers/users/create.php
+++ Http/controllers/users/create.php
@@ -2,14 +2,15 @@
 
 use Core\App;
 use Core\Database;
+use Core\Authentication as Auth;
+
+
+Auth::only_admin();
 
 $db = App::resolve(Database::class);
 $group_name = $db->query('select name,id from `groups`; ')->get();
 
-// var_dump($group_name);
-
 view("users/create.view.php", [
-   
     'group_name' => $group_name
 ]);
 
