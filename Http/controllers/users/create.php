24a1dbf81a6f8a0e78b4ec6e7aab09d31654798b
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-23 23:08:55 +0200

    fix users/create controller

--- Http/controllers/users/create.php
+++ Http/controllers/users/create.php
@@ -2,6 +2,11 @@
 
 use Core\App;
 use Core\Database;
+use Core\Authentication as Auth;
+
+
+Auth::only_admin();
+
 
 $db = App::resolve(Database::class);
 $group_name = $db->query('select name,id from `groups`; ')->get();
