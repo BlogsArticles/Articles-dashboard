ccb1d79763ad84b6147ed98fb183a7f09ccc2cd4
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-24 22:46:24 +0200

    add pagination to users page

--- Http/controllers/users/index.php
+++ Http/controllers/users/index.php
@@ -8,21 +8,37 @@ Auth::only_admin();
 
 $db = App::resolve(Database::class);
 
-if (isset($_GET['search'])) {
+$start=0;
+$offset=10;
+$max=0;
+$page=1;
 
-    $user = $db->query('select u.* ,g.name as group_name  from users u ,`groups` g where u.group_id= g.id and u.name like :name  and u.is_deleted is null;', [
-        'name' => "%" . $_GET['search'] . "%"
-
-    ])->get();
-
-    view("users/index.view.php", [
-        'users' => $user
-    ]);
-
+if(isset($_GET["page"]) && is_numeric($_GET["page"])){
+    $page=intval($_GET["page"]);
+    $start=($page-1)*$offset;
+    
 }
 
-$users = $db->query('select u.* ,g.name as group_name  from users u ,`groups` g where u.group_id= g.id and  u.is_deleted is null;')->get();
+if (isset($_GET['search'])) {
+    $count=$db->query('select count(*) from `users` where is_deleted is null and name like :search)',[
+        "search"=>'%'.$_GET["search"].'%']
+        )->find();
+    $max=ceil($count["count(*)"]/$offset);
+
+    $users = $db->query("select u.* ,g.name as group_name  from users u ,`groups` g where u.group_id= g.id and u.name like :name  and u.is_deleted is null LIMIT  $start,$offset;", [
+        'name' => "%" . $_GET['search'] . "%"
+    ])->get();
+
+}else{
+    $count=$db->query('select count(*) from `users` where is_deleted is null')->find();
+    $max=ceil($count["count(*)"]/$offset);
+
+    $users = $db->query("select u.* ,g.name as group_name  from users u ,`groups` g where u.group_id= g.id and  u.is_deleted is null LIMIT  $start,$offset;")->get();
+}
+
 
 view("users/index.view.php", [
-    'users' => $users
+    'users' => $users,
+    "max"=>$max,
+    "page"=>$page
 ]);
