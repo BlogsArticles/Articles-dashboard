e048ba679280585f761e417a320960990c9fa3da
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-24 22:50:53 +0200

    add pagination to articles page

--- views/articles/index.view.php
+++ views/articles/index.view.php
@@ -64,6 +64,17 @@
                             <?php endforeach; ?>
                             </tbody>
                         </table>
+                        <div class="col d-flex justify-content-end mt-2">
+                            <nav aria-label="Page navigation example col-10">
+                                <ul class="pagination pagination-md">
+                                    <li class="page-item"><a class="page-link" href="?page=<?php echo $page-1<1?1:$page-1;?>">Previous</a></li>
+                                    <?php for($i=1; $i<=$max; $i++){?>
+                                        <li class="page-item <?php echo $page==$i?'active':''?>"><a class="page-link" href="?page=<?=$i?>"><?=$i?></a></li>
+                                    <?php }?>
+                                    <li class="page-item"><a class="page-link" href="?page=<?php echo $page+1>$max?$max:$page+1;?>">Next</a></li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
         </div><!-- /.container-fluid -->
 
