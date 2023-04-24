b894a3d86e47810a09d93cb9275b83d4fff94af3
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-24 23:08:13 +0200

    refactor breadcrumbs in users and groups

--- views/groups/create.view.php
+++ views/groups/create.view.php
@@ -15,7 +15,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="/">Home</a></li>
-                        <li class="breadcrumb-item">Groups</li>
+                        <li class="breadcrumb-item">Create Groups</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
