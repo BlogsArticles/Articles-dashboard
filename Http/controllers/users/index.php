8aa596d617f7709cfcd39cb59a84efbfba35d4f2
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-04-24 23:01:15 +0200

    fix error in user pagination

--- Http/controllers/users/index.php
+++ Http/controllers/users/index.php
@@ -18,17 +18,15 @@ if(isset($_GET["page"]) && is_numeric($_GET["page"])){
     $start=($page-1)*$offset;
     
 }
-
 if (isset($_GET['search'])) {
-    $count=$db->query('select count(*) from `users` where is_deleted is null and name like :search)',[
-        "search"=>'%'.$_GET["search"].'%']
-        )->find();
+    $count=$db->query('select count(*) from `users` where is_deleted is null and name like :search',[
+        "search"=>'%'.$_GET["search"].'%'
+        ])->find();
     $max=ceil($count["count(*)"]/$offset);
 
     $users = $db->query("select u.* ,g.name as group_name  from users u ,`groups` g where u.group_id= g.id and u.name like :name  and u.is_deleted is null LIMIT  $start,$offset;", [
         'name' => "%" . $_GET['search'] . "%"
     ])->get();
-
 }else{
     $count=$db->query('select count(*) from `users` where is_deleted is null')->find();
     $max=ceil($count["count(*)"]/$offset);
